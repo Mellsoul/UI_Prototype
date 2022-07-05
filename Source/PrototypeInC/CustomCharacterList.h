a16a1bb9392d7973f0fd85e900b3a47196511b10
Mellsoul <106270637+Mellsoul@users.noreply.github.com>
2022-07-05 06:29:01 +0200

    1

---
+++ Source/PrototypeInC/CustomCharacterList.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CustomCharacterList.generated.h"
+
+
+class Ubutton;
+class UVerticalBox;
+class UHorizontalBox;
+class UScrollBox;
+class UImage;
+class UEditableText;
+class UTextBlock;
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPEINC_API UCustomCharacterList : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PrototypeInC/PrototypeInC.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PrototypeInC.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PrototypeInC, "PrototypeInC" );
